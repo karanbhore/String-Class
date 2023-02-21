596e775b812c24f950be52393763e0092c603717
karanbhore <karanbhore64@gmail.com>
2023-02-21 18:55:38 +0530

    file adding

---
+++ src/com/prowings/practise/IntegerDemo1.java
@@ -0,0 +1,27 @@
+package com.prowings.practise;
+
+public class IntegerDemo1 {
+
+	public static void main(String[] args) {
+		Integer i1=128;
+		Integer i2=128;
+		
+		Integer i3=-127;
+		Integer i4=-127;
+		
+		Integer i5=321;
+		Integer i6=321;
+		
+		System.out.println(i1==i2);
+		System.out.println(i1.equals(i2));
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+}
